a7cd608541255207c1d5fc151b1f086ba24c78a6
Thaila Esteves <thailalestevesl@gmail.com>
2024-08-21 23:43:04 -0300

    models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create([
+  {name: "Ciao", address: "123 Pizza Street", phone_number: "2398-8972", category: "italian"},
+  {name: "Bonjour", address: "123 Paris Street", phone_number: "2347-1234", category: "french"},
+  {name: "Tokyo", address: "101 Sushi Street", phone_number: "3192-4456", category: "japanese"},
+  {name: "Bistro", address: "123 Lyon Street", phone_number: "4321-8858", category: "french"},
+  {name: "Waffle Restaurant", address: "123 Belgium Street", phone_number: "2248-8893", category: "belgian"}
+])
